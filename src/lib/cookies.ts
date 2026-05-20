2bcd0ec557379c95adb515fe97b8727d80e5eca1
Air091 <acesparkle243@gmail.com>
2026-05-20 08:17:51 +0800

    added cookies

---
+++ src/lib/cookies.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import type { Response } from "express";
+
+dotenv.config();
+
+const isProduction: boolean = process.env.config === "production";
+
+export function setRefreshTokenCookie(response: Response, token: string) {
+  return response.cookie("refreshToken", {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 3, // 3days
+  });
+}
+
+export function clearRefreshTokenCookie(response: Response) {
+  return response.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: "lax",
+    path: "/",
+  });
+}
