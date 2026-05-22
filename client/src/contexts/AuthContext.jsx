2f9118ef35a6b82b64d7bf36a9d3858e386eed29
Air091 <acesparkle243@gmail.com>
2026-05-22 09:43:44 +0800

    setup client

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,8 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+}
